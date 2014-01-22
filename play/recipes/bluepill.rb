04da14b4f558627ff3fb6ca140eb712261e9bedd
Depech Vogel <depechvogelnl@gmail.com>
2014-01-22 22:37:38 +0000

    --..

--- play/recipes/bluepill.rb
+++ play/recipes/bluepill.rb
@@ -1,8 +1,5 @@
 node[:deploy].each do |application, deploy|
     Chef::Log.info(">>>>>>>>>>>>>>>>>>>>>>> #{deploy[:deploy_to]}")
-    owner 'root'
-    group 'root'
     command 'bundle exec bluepill --no-privileged load config/bluepill.pill'
-    cwd "#{node[:deployed_to_path]}/current"
 end
 
