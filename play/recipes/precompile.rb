ff3352eea5a4ed78e22974dc98b97d2c37703a86
Lokhart <fpenta@gmail.com>
2014-02-03 11:16:54 +0000

    Update precompile.rb

--- play/recipes/precompile.rb
+++ play/recipes/precompile.rb
@@ -1,7 +1,7 @@
 execute 'rake assets:precompile' do
   cwd "/srv/www/ancl_ruby-1/current"
   user 'deploy'
-  group 'www-data'
+  group 'nginx'
   command 'bundle exec rake assets:precompile'
   environment 'RAILS_ENV' => 'development'
 end
